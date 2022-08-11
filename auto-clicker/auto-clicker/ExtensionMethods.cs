56a7b5bb9615071fd037907be2e25f5d4b02cd64
Henrique Andrade <henrique.andrade22@hotmail.com>
2022-08-11 09:10:25 -0300

    Create TakeToArray extension method

---
+++ auto-clicker/auto-clicker/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace auto_clicker
+{
+    public static class ExtensionMethods
+    {
+        public static string[] TakeToArray(this IEnumerable<string> collection, int max) =>
+            collection
+                .Take(max)
+                .ToArray();
+    }
+}
